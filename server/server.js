aa092bc5e1aadd32a3c6f553d5953245a4712aa3
Marcos Andrade <mnandrade1999@gmail.com>
2023-11-03 16:29:59 -0500

    made some notes for how I will tackle project

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static('server/public'));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
+let calculations = [] // an object containing num1,num2, and operation
 
 
 // Here's a wonderful place to make some routes:
